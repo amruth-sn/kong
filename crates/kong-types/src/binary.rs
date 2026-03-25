b8b2d2967bcb79ea3a46fc2037105250baeb3aea
Amruth <amruth@bu.edu>
2026-03-25 00:06:19 -0400

    update display names using strum

--- crates/kong-types/src/binary.rs
+++ crates/kong-types/src/binary.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
-use serde_json::to_string;
+use strum_macros::AsRefStr;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize, AsRefStr)]
 #[serde(rename_all = "snake_case")]
+#[strum(serialize_all = "snake_case")]
 pub enum Arch {
     X86_64,
     X86,
@@ -14,14 +15,8 @@ pub enum Arch {
     PowerPc,
 }
 
-impl Arch {
-    pub fn model_name(&self) -> String {
-        to_string(self).unwrap()
-    }
-}
-
 impl std::fmt::Display for Arch {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.model_name())
+        f.write_str(self.as_ref())
     }
 }
