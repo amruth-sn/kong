f6e68118fd7e6e0598e00f6525f4f9ea77460203
Amruth <amruth@bu.edu>
2026-03-25 21:05:52 -0400

    implement xda model with candle

--- crates/kong-ml/src/main.rs
+++ crates/kong-ml/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use safetensors::SafeTensors;
+use std::io::Error;
+fn main() -> Result<(), Error> {
+    let paths = vec![
+        "../../tools/xda/models/xda_x86_64.safetensors",
+        "../../tools/xda/models/xda_aarch64.safetensors",
+        "../../tools/xda/models/xda_arm.safetensors",
+        "../../tools/xda/models/xda_riscv64.safetensors",
+    ];
+    for path in paths {
+        let print_path = path.split("/").last().unwrap();
+        println!("==========={print_path}===========");
+        let buffer = &std::fs::read(path)?;
+        let tensors = SafeTensors::deserialize(buffer).unwrap();
+        for name in tensors.names() {
+            println!("{name}");
+        }
+    }
+    Ok(())
 }
